79b256e2015295e1c104ef4c6e50510e5e633179
jmcragun <jmcragun@gmail.com>
2016-07-14 09:30:21 -0600

    Buggy code

---
+++ TheGame/website3/scripts/openingScript.js
@@ -0,0 +1,27 @@
+var nextCounter = 0;
+var userName = "";
+var nameAccepted = false;
+$(document).ready(function () {
+	$("#whatsYourName").hide();
+	$("#next").click(function () {
+		if (nextCounter == 0) {
+			$("#whatsYourName").fadeIn(1000);
+			nextCounter++;
+		}
+		else if (nextCounter == 1) {
+			userName = $("#whatsYourName").val();
+			if (userName.length == 0) {
+				alert("Come on now, ye do have a name, don't ye?");
+			} else {
+				var nameAccepted = function () {
+					confirm("So, yer name is " + userName + "?");
+				}
+				if (nameAccepted == true) {
+					$("#greeting").fadeOut(1000);
+					$("<p>Well then, " + userName + ", a world in turmoil rests at your feet. Will you save it, or shall you destroy it?</p>").hide().appendTo("body").fadeIn(1000);
+					nextCounter++;
+				}
+			}
+		}
+	});
+});
